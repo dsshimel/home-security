a970436b2f8d6616acc58c62cc7e755bebd4c07b
David Shimel <dsshimel@gmail.com>
2024-06-10 20:52:18 -0400

    Add skeleton Python files for camera and webserver

---
+++ camera.py
@@ -0,0 +1,12 @@
+"""Opens any attached camera(s) and streams frames to the server."""
+
+class Camera:
+    def __init__(self) -> None:
+        pass
+
+    def run(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    camera = Camera()
+    camera.run()
---
+++ server.py
@@ -0,0 +1,12 @@
+"""Aggregates camera streams and serves a React app to view them."""
+
+class Server:
+    def __init__(self) -> None:
+        pass
+
+    def run(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    server = Server()
+    server.run()
