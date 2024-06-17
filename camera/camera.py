c17f8bf0bcd4f7fc2674c460785f05afa6bb8d4f
David Shimel <dsshimel@gmail.com>
2024-06-17 18:11:28 -0400

    Clarify use of UDP socket

--- camera/camera.py
+++ camera/camera.py
@@ -10,13 +10,14 @@ class Camera:
         pass
 
     def run(self) -> None:
-        # TODO: Get all the cameras attached to the system.
+        # TODO: Get all the cameras attached to the system, not just the first one.
         cap = cv2.VideoCapture(0)
 
         while True:
             # Capture frame-by-frame
             # TODO: Stream this frame to the server.
-            ret, frame = cap.read()
+            # `frame` is a 2D array of RGB values (so technically a 3D array) each with a range of 0-255.
+            _, frame = cap.read()
 
             # Display the resulting frame
             cv2.imshow('Webcam', frame)
@@ -24,6 +25,10 @@ class Camera:
             # Break the loop on 'q' key press
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
+    def _send_frame(frame) -> None:
+        # Send the frame to the frame aggregation server.
+        pass
 
 if __name__ == "__main__":
     camera = Camera()
--- server/camera_frames_aggregator.py
+++ server/camera_frames_aggregator.py
@@ -1,38 +1,62 @@
 """Aggregates camera streams and serves a React app to view them."""
 
+# TODO: Consider using socketserver.UDPServer instead.
 import socket
 
 class CameraFramesAggregator:
 
     DEFAULT_ROUTER_IP = '192.168.1.1'
+    DEFAULT_NETWORK_BROADCAST_IP = '192.168.1.255'
+    DEFAULT_SERVER_IP = '192.168.1.156'
+    LOCALHOST = '127.0.0.1'
     PORT_HTTP = 80
     PORT_HTTPS = 443
+    PORT_LISTEN_DEFAULT = 42069
 
     def __init__(self) -> None:
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.__ip_address = self._get_ip_address()
+
+        # AF_INET is the address family for IPv4. SOCK_DGRAM is the socket type for UDP.
+        # self.__socket_broadcaster = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # self.__socket_broadcaster.sendto(b'Hello, world!', (CameraFramesAggregator.DEFAULT_NETWORK_BROADCAST_IP, CameraFramesAggregator.PORT_LISTEN_DEFAULT))
+
+        self.__socket_receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__socket_receiver.bind((self.__ip_address, CameraFramesAggregator.PORT_LISTEN_DEFAULT))
+        # Since this socket is for UDP, it is connectionless and therefore does not need to listen().
+        # See https://stackoverflow.com/questions/8194323/why-the-listen-function-call-is-not-needed-when-use-udp-socket.
+        # See https://superuser.com/questions/1096504/why-udp-does-not-show-listening-in-the-state-column-in-netstat
+
+        # print(self.__socket_receiver.getsockname())
 
     def run(self) -> None:
         # TODO: Broadcast this device's IP address to 192.168.1.255 so that clients can discover it
         # TODO: Spawn a new thread to listen on the socket for camera frames from the clients
-        self._cleanup()
+        try:
+            while True:
+                pass
+        finally:
+            self._cleanup()
 
     @property
     def device_ip(self):
         return self.__ip_address
 
     def _get_ip_address(self) -> str:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
-            # Connect to the router's IP address
-            self.__socket.connect((CameraFramesAggregator.DEFAULT_ROUTER_IP, 80))
-            ip_address = self.__socket.getsockname()[0]
+            # Connect this socket to the router's IP address (though any IP would work for our purpose here).
+            # This causes the socket to be bound, but we're not going to use it after this.
+            s.connect((CameraFramesAggregator.DEFAULT_ROUTER_IP, CameraFramesAggregator.PORT_HTTP))
         except Exception as e:
-            ip_address = 'Unable to determine IP address'
-        return ip_address
+            return 'Unable to determine IP address'
+        else:
+            return s.getsockname()[0]
+        finally:
+            s.close()
 
     def _cleanup(self) -> None:
-        self.__socket.close()
+        # self.__socket_broadcaster.close()
+        self.__socket_receiver.close()
 
 if __name__ == '__main__':
     server = CameraFramesAggregator()
